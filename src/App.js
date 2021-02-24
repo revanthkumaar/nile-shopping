3fac50642152577a9014a999c8bb72ce53332bbd
revanthkumaar <iamrevanthkumar@gmail.com>
2021-02-24 12:40:24 +0530

    routing example
    routing installation and implementation on the home page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 //Get homepage component to main component
 import HomePage from './pages/homepage/homepage.component';
+import {Route} from 'react-router-dom';
 
 
+const HatsPage = () => (
+  <div>
+    <h1>this is hats page</h1>
+  </div>
+)
+//localhost:3000
 //RENDERING
 function App() {
   return (
     <div>
-      <HomePage />    {/* render homepage component here*/}
-      <h1>Testing the oswald font</h1>
+      <Route path="/" component={HomePage}/>
+      <Route path="/hats" component={HatsPage}/>
     </div>
   );
 }

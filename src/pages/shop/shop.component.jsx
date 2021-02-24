bf1175903cf50fa8a2eb41996210ff03ba6794ba
revanthkumaar <iamrevanthkumar@gmail.com>
2021-02-24 15:48:33 +0530

    shop data included
    commited changes to shop data

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//class component as we need to deal with data/state
+class shopPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            collections: [
+    
+            ]
+        }
+    }
+  
+}

04b5819c58aa8b7ff2e300b10ccb00cecb49851f
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-01 11:50:50 +0530

    cart reducers
    cart reducers

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import {connect} from 'react-redux';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className = "header" >
         <Link className="logo-container" to="/">
             <Logo className="logo"/>
@@ -25,14 +25,18 @@ const Header = ({currentUser}) => (
             }
         <CartIcon/>
         </div>
-        <CartDropdown/>
+        {
+            hidden ? null : <CartDropdown/>
+        }
+        
     </div>
     
 )
 
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 

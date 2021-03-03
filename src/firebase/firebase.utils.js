9abd35f2593382431c70b0f3d01b6dfaf9279e6f
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-03 10:37:28 +0530

    alksdjlkj
    ldsfjalks

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,7 +5,6 @@ import 'firebase/auth';
 
 
 //get the config from firebase app on the cloud server
-
 var config = {
     apiKey: "AIzaSyBx4vN9Xw974R7oadCqpjZNHHI9UzqWoHA",
     authDomain: "crwn-db-24119.firebaseapp.com",
@@ -15,3 +14,17 @@ var config = {
     appId: "1:492740632298:web:4aeeaeaf8db726522b0193",
     measurementId: "G-1299T52Z43"
   };
+
+  //initializeApp is a function to establish connection
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt:'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export default firebase;
+
+
+

acab72378b4089f33c7577574bbb13cc9d6b34b4
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-03 10:32:54 +0530

    start of firebase auth
    firebase auth

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,17 @@
+//import firebase dependencies or modules
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+//get the config from firebase app on the cloud server
+
+var config = {
+    apiKey: "AIzaSyBx4vN9Xw974R7oadCqpjZNHHI9UzqWoHA",
+    authDomain: "crwn-db-24119.firebaseapp.com",
+    projectId: "crwn-db-24119",
+    storageBucket: "crwn-db-24119.appspot.com",
+    messagingSenderId: "492740632298",
+    appId: "1:492740632298:web:4aeeaeaf8db726522b0193",
+    measurementId: "G-1299T52Z43"
+  };

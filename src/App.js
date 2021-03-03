72518535da1030f182b41060cc3381c6bfd1ad94
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-03 12:11:16 +0530

    ahsdlkh
    ljlaskhdl

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 //Get homepage component to main component
 import HomePage from './pages/homepage/homepage.component';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import {auth} from './firebase/firebase.utils'
 
 
-//RENDERING
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Route exact path="/" component={HomePage}/>
-      <Route exact path="/shop" component={ShopPage}/>
-      <Route exact path="/signin" component={SignInAndSignUpPage}/>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(){
+    super();
+
+    this.state = { //initialized the state
+      currentUser:null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser:user});
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+    <Switch> <Route exact path="/" component={HomePage}/>
+        <Route exact path="/shop" component={ShopPage}/>
+        <Route exact path="/signin" component={SignInAndSignUpPage}/> </Switch>
+       
+      </div>
+    );
+  }
+    
+
 }
+  
+
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,44 +1,35 @@
 import React from 'react';
-import {Link} from 'react-router-dom' //used for routing to the pages
-import './header.styles.scss'
-import {ReactComponent as Logo} from '../../assets/crown.svg' //getting logo file
-import CartIcon from '../cart-icon/cart-icon.component'
-import {connect} from 'react-redux';
-//functional component (as there is no data/state that is to be changed)
-import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+import { Link } from 'react-router-dom';
 
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = ({currentUser, hidden}) => (
-    <div className = "header" >
-        <Link className="logo-container" to="/">
-            <Logo className="logo"/>
-        </Link>
-        <div className="options">
-            <Link className="option" to="/shop">SHOP </Link>  
-            <Link className="option" to="/shop">CONTACT </Link> 
-            {
-                currentUser ? (
-                    <div className="option">SIGN OUT</div>
-                ) : (
-                    <Link className="option" to='/signin'>SIGN IN</Link>
-                )
-            }
-        <CartIcon/>
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
+
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
-        {
-            hidden ? null : <CartDropdown/>
-        }
-        
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
     </div>
-    
-)
+  </div>
+);
 
-
-const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
-    currentUser,
-    hidden
-});
-
-
-export default connect(mapStateToProps)(Header);
-
+export default Header;
